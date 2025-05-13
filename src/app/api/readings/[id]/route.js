baf4ea6d775a7fccf85eaf7ce051463e7f319005
krvdd <211356044+krvdd@users.noreply.github.com>
2025-05-13 16:39:31 +0200

    minor

--- src/app/api/readings/[id]/route.js
+++ src/app/api/readings/[id]/route.js
@@ -3,12 +3,12 @@ import { get_readings, put_readings } from '@/data.js'
 
 export async function GET(req, {params}) {
 	const {id} = await params;
-	return NextResponse.json(await get_readings(id));
+	return NextResponse.json(get_readings(id));
 }
 
 export async function PUT(req, {params}) {
 	const {id} = await params;
 	const data = await req.json();
-	await put_readings(id, data);
+	put_readings(id, data);
 	return NextResponse.json('success');
 }
